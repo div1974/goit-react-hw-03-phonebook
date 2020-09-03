7259a196149703cba969f41b56a207f9aa09e8c1
div1974 <div@ukr.net>
2020-09-04 00:45:49 +0300

    Updates

---
+++ precache-manifest.1545af0010b376e1dbbb0777ef0d04a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ab250fccd2794bfc98e5e2588d685f",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "f8006313eb73c179bf05",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.68b278aa.chunk.css"
+  },
+  {
+    "revision": "ab7d12432f4e5add68b2",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.241c8151.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.241c8151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8006313eb73c179bf05",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.4c9ad741.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
